024db0512a1a0a3f993b590a4b452f930e67789a
Bayo <jbayo@iesebre.com>
2022-12-14 18:57:15 +0100

    Final Video 11 Casteaching, abansem amb la tipografia

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,17 +1,19 @@
 <x-casteaching-layout>
-    <div class="flex flex-col h-screen">
+    <div class="flex flex-col space-x-4 space-y-4 lg:space-x-6 lg:space-y-4 xl:space-x-15 lg:space-y-5
+    2xl:space-x-20 2xl:space-y-10 items-center">
         <iframe
-            class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:p-10 h-4/5"
+            class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:p-10 h-4/5 w-full"
+            style="height: 80vh;"
             src="https://www.youtube.com/embed/6O4mPpj6rI8"
             title="YouTube video player" frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen></iframe>
 
-        <div class="p-4 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 xl:py-20">
+        <h2 class="text--grey-900 uppercase font-bold text-2xl tracking-tight">
             {{ $video->title }}
-        </div>
-        <div class="p-4 lg:p-5 xl:p-10 xl:py-5 2xl:px-20 xl:py-20">
-            {{ $video->description }}
+        </h2>
+        <div class="prose-sm md:prose lg-prose-xl 2xl:prose-2xl">
+            {!!Str::markdown($video->description) !!}
         </div>
     </div>
 </x-casteaching-layout>
