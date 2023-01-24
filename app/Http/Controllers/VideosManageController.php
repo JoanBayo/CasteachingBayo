74cf14745bc944b80aab4abddc796291da676167
Joan Bayo <91154202+JoanBayo@users.noreply.github.com>
2023-01-24 12:55:10 +0100

    Update VideosManageController.php

--- app/Http/Controllers/VideosManageController.php
+++ app/Http/Controllers/VideosManageController.php
@@ -50,7 +50,7 @@ class VideosManageController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('videos.manage.edit',['video' => Video::findOrFail($id) ]);
     }
 
     /**
@@ -58,7 +58,14 @@ class VideosManageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $video = Video::findOrFail($id);
+        $video->title = $request->title;
+        $video->description = $request->description;
+        $video->url = $request->url;
+        $video->save();
+
+        session()->flash('status', 'Successfully updated');
+        return redirect()->route('manage.videos');
     }
 
     /**
