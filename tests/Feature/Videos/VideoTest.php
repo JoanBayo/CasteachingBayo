05bab0470434280c8a01870a5825587d604509b0
Joan Bayo <91154202+JoanBayo@users.noreply.github.com>
2023-01-24 13:05:24 +0100

    Update VideoTest.php

--- tests/Feature/Videos/VideoTest.php
+++ tests/Feature/Videos/VideoTest.php
@@ -36,6 +36,7 @@ class VideoTest extends TestCase
         $response->assertSee('Ubuntu 101');
         $response->assertSee('Here description');
         $response->assertSee('22 de novembre de 2022');
+        $response->assertSee('https://youtu.be/w8j07_DBl_I');
 
 
     }
