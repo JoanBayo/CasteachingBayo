cce11f6896329d6bfc4a3daa234834f51b01d16e
Bayo <jbayo@iesebre.com>
2023-03-16 17:24:45 +0100

    produccio

--- config/casteaching.php
+++ config/casteaching.php
@@ -6,5 +6,8 @@ return[
      'name' => env('DEFAULT_USER_NAME','Joan Bayo Benito'),
      'email' => env('DEFAULT_USER_EMAIL','jbayo@iesebre.com'),
      'password' => env('DEFAULT_USER_PASSWORD','12345678'),
- ]
+ ],
+    'admins' => [
+        'jbayo@iesebre.comgut'
+    ]
 ];
