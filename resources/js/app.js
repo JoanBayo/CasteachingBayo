6b4ebab23b108e4a1c3287d13496b535ede66ff2
Bayo <jbayo@iesebre.com>
2023-03-22 17:58:45 +0100

    prova produccio

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ import VideoForm from "./components/VideoForm.vue";
 import casteaching from '@acacha/casteaching'
 import Status from "./components/Status.vue";
 
-window.api = casteaching({baseUrl: 'http://casteachingbayo.test/api/'})
+window.api = casteaching({baseUrl: 'https://casteachingbayo.test/api/'})
 window.api.setToken('4dtQl58nAQ6ciWW04BHdQCav2WbMnspuJnHjGYrO')
 
 
