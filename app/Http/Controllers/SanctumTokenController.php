4673f22c350f37c69b1936cc20dc4e45fb94ff8f
Bayo <jbayo@iesebre.com>
2023-03-23 18:36:57 +0100

    prova produccio

--- app/Http/Controllers/SanctumTokenController.php
+++ app/Http/Controllers/SanctumTokenController.php
@@ -18,9 +18,7 @@ class SanctumTokenController extends Controller
     public function __invoke(Request $request)
     {
         $request->validate([
-            'email' => 'required',
-            'password' => 'required',
-            'device_name' => 'required',
+
         ]);
 
         $user = User::where('email', $request->email)->first();
