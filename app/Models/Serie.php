e583e9b0810320092671bf9785722d98fd0f6677
Joan Bayo <91154202+JoanBayo@users.noreply.github.com>
2023-03-31 10:52:58 +0200

    Canvis a series

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Serie extends Model
 {
     use HasFactory;
+    
+    public static function testedBy()
+    {
+        return SerieTest::class;
+    }
+
+    protected $guarded = [];
+
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
+
+    // formatted_created_at accessor
+    public function getFormattedCreatedAtAttribute()
+    {
+        if(!$this->created_at) return '';
+        $locale_date = $this->created_at->locale(config('app.locale'));
+        return $locale_date->day . ' de ' . $locale_date->monthName . ' de ' . $locale_date->year;
+    }
+
+    public function getFormattedForHumansCreatedAtAttribute()
+    {
+        return optional($this->created_at)->diffForHumans(Carbon::now());
+    }
+
+    public function getCreatedAtTimestampAttribute()
+    {
+        return optional($this->created_at)->timestamp;
+    }
 }
