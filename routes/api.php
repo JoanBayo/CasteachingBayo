9d2fd3838219beb5e8decdde2476795d3e5285a3
Bayo <jbayo@iesebre.com>
2023-05-03 16:49:32 +0200

    prova2

--- routes/api.php
+++ routes/api.php
@@ -35,4 +35,5 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 Route::post('/sanctum/token',\App\Http\Controllers\SanctumTokenController::class);
 
 Route::get('/series', [ SeriesApiController::class,'index']);
+
 Route::get('/series/{id}', [ SeriesApiController::class,'show']);
